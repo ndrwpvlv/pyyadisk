d7b1b1d358bca897bbf88be7ac7c54b869588695
Andrei Pavlov <ndrw.pvlv@gmail.com>
2021-08-13 05:08:12 +0300

    Class and methods documentation update

--- pyyadisk/yandexdisk.py
+++ pyyadisk/yandexdisk.py
@@ -8,7 +8,50 @@ from .helpers import filter_dict_by_key
 
 
 class YandexDisk:
+    """Yandex Disk Rest API V1 wrapper
+
+    Python realisation of Yandex Disk Rest API V1 wrapper:
+    - OAuth authorization by token
+    Resources:
+    - Get metadata of file or directory
+    - Create directory
+    - Delete of file or directory
+    - Move of file or directory
+    - Copy of file or directory
+    - Get download link (private)
+    - Share directory or file and get public link
+    - Get download link (public)
+    - Get list of files
+    - Get last uploaded files
+    - Upload file
+    - Upload file by url
+    Trash:
+    - Empty
+    - Delete files
+    - Restore files
+    Async operations status
+
+    Attributes:
+        token: Oauth token (get in at https://yandex.ru/dev/disk/poligon/)
+        headers: Dictionary with headers ('Authorization', 'Accept')
+        proxies: (optional) Dictionary with proxy addresses for http and https
+        session: Object of requests.Session()
+        ssl_verify: (optional) Flag of connection ssl verification check
+        uri: Yandex Disk Rest API endpoint uri
+        resources: Yandex Disk Rest API resources uri
+        params: Dictionary to send in the query string for the Request
+    """
+
     def __init__(self, token: str = None, proxy: str = None, ssl_verify: bool = True, max_retries: int = 5):
+        """
+        Inits of YandexDisk REST API V1 wrapper class
+
+        Args:
+            token: Oauth token (get in at https://yandex.ru/dev/disk/poligon/)
+            proxy: (optional) Proxy address for http and https
+            ssl_verify: (optional) Flag of connection ssl verification check
+            max_retries: (optional) Number of maximum connection retries
+        """
         self.token = token
         self.headers = {'Authorization': 'OAuth {}'.format(self.token), 'Accept': 'application/json'}
 
@@ -28,52 +71,247 @@ class YandexDisk:
         self.params = {'path': None, 'fields': None, 'sort': None, 'limit': None, 'offset': None}
 
     def trash(self, path: str = None):
+        """
+        Enter to the Trash mode.
+
+        Typical usage example:
+            disk = YandexDisk()
+            trash_ = disk.trash('path/to/the/file')
+
+        Args:
+            path: The full path to the resource in the Trash (file or directory). For the root of trash use path = '/' or None
+
+        Returns:
+            Self YandexDisk object
+        """
         self.resources = TRASH_PATH
         self.params['path'] = path
         return self
 
     def restore(self, name: str = None, force_async: bool = None, overwrite: bool = False):
+        """
+        Restore trash items
+
+        Typical usage example:
+            disk = YandexDisk()
+            trash_ = disk.trash('path/to/the/file.pdf')
+            response = trash_.restore()
+
+        Args:
+            name: The name under which the resource will be restored.
+            force_async: Execute asynchronously (True or False).
+            overwrite: Overwrite the existing resource with the restored one  (True or False).
+
+        Returns:
+            Tuple with Response code and dictionary from JSON:
+            (Response code, JSON Response dict or None for error)
+
+            JSON Response dict:
+                {
+                  "href": "string",
+                  "method": "string",
+                  "templated": true
+                }
+        """
         params = {**self.params, 'name': name, 'force_async': force_async, 'overwrite': overwrite, }
         return self._put(f'{self.resources}/restore', params=filter_dict_by_key(params))
 
     def operations(self, operation_id: str):
+        """
+        Get the status of an asynchronous operation
+
+        Typical usage example:
+            disk = YandexDisk()
+            response = disk.operations('aabbccdd')
+
+        Args:
+            operation_id: Operation Id
+
+        Returns:
+            Tuple with Response code and dictionary from JSON:
+            (Response code, JSON Response dict or None for error)
+
+            JSON Response dict:
+                {
+                  "status": "string"
+                }
+        """
         params = {'operation_id': operation_id}
         return self._get(f'{OPERATIONS_PATH}/{operation_id}', params=params)
 
     def path(self, path: str = None):
+        """
+        Enter to the Disk mode
+
+        Typical usage example:
+            disk = YandexDisk()
+            path_ = disk.path('path/to/the/file.pdf')
+
+        Args:
+            path: The full path of the Disk resource (file or directory)
+
+        Returns:
+            Self YandexDisk object
+        """
         self.resources = RESOURCE_PATH
         self.params['path'] = path
         return self
 
     def fields(self, fields: str = None):
+        """
+        Add filter fields of JSON elements.
+
+        Typical usage example:
+            disk = YandexDisk()
+            path_ = disk.path('path/to/the/file.pdf')
+            path_.fields(fields='_embedded.items.name,_embedded.items.size')
+
+        Args:
+            fields: string with fields names. For example fields = '_embedded.items.name,_embedded.items.size'
+
+        Returns:
+            Self YandexDisk object
+        """
         self.params['fields'] = fields
         return self
 
-    def sort(self, sort):
+    def sort(self, sort: str = None):
+        """
+        Set an attribute by which to sort the list of resources:
+        - name
+        - path
+        - created
+        - modified
+        - size
+
+        Typical usage example:
+            disk = YandexDisk()
+            path_ = disk.path('path/to/the/file.pdf')
+            path_.sort(sort='name')
+
+        Args:
+            sort: The attribute by which to sort the list of resources
+
+        Returns:
+            Self YandexDisk object
+        """
         self.params['sort'] = sort
         return self
 
     def add_param(self, key, value):
+        """
+        Add any parameter to request params dictionary
+
+        Typical usage example:
+            disk = YandexDisk()
+            disk.add_param('sort': 'name')
+
+        Args:
+            key: The key of parameter
+            value: The value of parameter
+
+        Returns:
+            Self YandexDisk object
+        """
         self.params[key] = value
         return self
 
     def add_params(self, params: dict):
+        """
+        Add dictionary with parameters to request params dictionary
+
+        Typical usage example:
+            disk = YandexDisk()
+            disk.add_params({'sort': 'name', })
+
+        Args:
+            params: Dictionary with parameters
+
+        Returns:
+            Self YandexDisk object
+        """
         self.params.update(params)
         return self
 
     def get(self, limit: int = None, offset: int = None):
+        """
+        Make get metadata of file or directory from Disk or Trash mode
+
+        Typical usage example:
+            disk = YandexDisk()
+            info = disk.path('path/to/the/file').get()
+
+        Args:
+            limit: The number of embedded resources to return
+            offset: Offset from the beginning
+
+        Returns:
+            Tuple with Response code and dictionary from JSON:
+            (Response code, JSON Response dict or None for error)
+        """
         params = {**self.params, 'limit': limit, 'offset': offset, }
         return self._get(self.resources, params=filter_dict_by_key(params))
 
     def create(self, subdir: str = None):
+        """
+        Make directory or subdirectory by the path
+
+        Typical usage example:
+            disk = YandexDisk()
+            directory = disk.path('path/to/the/directory').create()  # create the directory by the path
+            directory = disk.path('path/to/the/directory').create('subdirectory')  # create the subdirectory in the directory
+
+        or
+
+            disk = YandexDisk()
+            directory = disk.path('path/to/the/directory')
+            subdir_list = ['sub_1', 'sub_2', 'sub_3', 'sub_4', ]
+            for s in subdir_list:
+                directory.create(s)
+
+        Args:
+            subdir: Name of subdirectory. If 'subdir = None' the directory will be created by path-data
+
+        Returns:
+            Tuple with Response code and dictionary from JSON:
+            (Response code, JSON Response dict or None for error)
+        """
         params = {**self.params, 'path': f'{self.params["path"]}/{subdir}' if subdir else self.params["path"], }
         return self._put(self.resources, params=filter_dict_by_key(params))
 
     def delete(self, force_async: bool = None, md5_hash: str = None, permanently: bool = False):
+        """
+        Delete file or directory by path from Disk or Trash mode
+
+        Typical usage example:
+            disk = YandexDisk()
+            directory = disk.path('path/to/the/directory').delete()
+
+        Args:
+            force_async: Execute asynchronously (True or False).
+            md5_hash: md5 hash of file
+            permanently: Flag of permanently delete
+
+        Returns:
+            Tuple with Response code and dictionary from JSON:
+            (Response code, JSON Response dict or None for error)
+        """
         params = {**self.params, 'force_async': force_async, 'md5': md5_hash, 'permanently': permanently, }
         return self._delete(self.resources, params=filter_dict_by_key(params))
 
     def copy(self, destination: str, force_async: bool = None, overwrite: bool = None):
+        """
+        Copy file or directory to new destination
+
+        Args:
+            destination: Destination path
+            force_async: Execute asynchronously (True or False).
+            overwrite: Flag of overwrite enable
+
+        Returns:
+            Tuple with Response code and dictionary from JSON:
+            (Response code, JSON Response dict or None for error)
+        """
         params = {'from': self.params['path'], 'path': destination, 'fields': self.params['fields'],
                   'force_async': force_async, 'overwrite': overwrite, }
         return self._post(f'{self.resources}/copy', params=filter_dict_by_key(params))
